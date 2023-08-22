59e3b28a21580975398ae96a4ac4388809096525
01JAMIL <jamilbenbrahim1@gmail.com>
2023-08-22 22:18:06 +0100

    Few changes

--- src/locofy/screens/NewBusinessPlan.tsx
+++ src/locofy/screens/NewBusinessPlan.tsx
@@ -5,11 +5,11 @@ import Questions from '../../components/Questions'
 const NewBusinessPlan = () => {
     return (
         <div className='flex items-center justify-center bg-purple-light min-h-[calc(100%_-_65px)] px-[20px] py-[40px] font-plus-jakarta-sans'>
-            <div className='px-[10px] xl:px-[100px] grid grid-cols-1 lg:grid-cols-2 gap-[10%]'>
-                <div className=''>
+            <div className='px-[10px] xl:px-[100px] grid grid-cols-1 lg:grid-cols-2 gap-[80px]'>
+                <div className='2xl:px-[10%]'>
                     <Stepper />
                 </div>
-                <div className=''>
+                <div className='2xl:px-[10%]'>
                     <Questions />
                 </div>
             </div>
