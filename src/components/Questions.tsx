effa7280ef56033a6e07b256ea5b7397ed9a9a8e
Ahmed Hlel <kirigaya.kazuto.hlel@gmail.com>
2023-08-27 21:28:01 +0100

    Questions Modifications and other changes

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -40,9 +40,6 @@ const Questions = () => {
   const dispatch = useAppDispatch();
   const currentStep = useAppSelector((state) => state.stepper.currentStep);
   const section = useAppSelector((state) => state.questions.section);
-  const [currentBlockIndex, setCurrentBlockIndex] = useState(0);
-  const [currentNestedBlockIndex, setCurrentNestedBlockIndex] = useState(0);
-  const [mainBlockQuestionsShown, setMainBlockQuestionsShown] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const questionsPerPage = 5;
   const [isBackDisabled, setIsBackDisabled] = useState(true);
@@ -52,107 +49,90 @@ const Questions = () => {
   const currentBusinessPlan = useAppSelector(
     (state) => state.businessPlan.currentBusinessPlan
   );
-  // console.log("Current Business Plan :", currentBusinessPlan);
 
   const currentBusinessPlanId = currentBusinessPlan?.id;
-  // console.log("Current Business Plan ID:", currentBusinessPlanId);
-
-  // const [answers, setAnswers] = useState<Record<string, any>>({});
   const answers = useAppSelector((state) => state.answers.answers);
 
   useEffect(() => {
     dispatch(fetchSectionsForStep(currentStep));
   }, [currentStep, dispatch]);
 
-  const currentBlock = section?.blocks?.[currentBlockIndex];
-  const nestedBlocks = currentBlock?.blocks || [];
-  const isInsideNestedBlock =
-    mainBlockQuestionsShown &&
-    nestedBlocks.length > 0 &&
-    currentNestedBlockIndex < nestedBlocks.length;
-  const blockToRender = isInsideNestedBlock
-    ? nestedBlocks[currentNestedBlockIndex]
-    : currentBlock;
+  // -----------------------------------------------------------------------------------------------------------------------------------
 
-  const totalQuestions = blockToRender?.questions.length || 0;
-  const totalPages = Math.ceil(totalQuestions / questionsPerPage);
+  const [currentIndexList, setCurrentIndexList] = useState([0]);
+
+  const getBlockToRender = (
+    blockIndex: number,
+    nestedIndices: Array<number> = []
+  ): any => {
+    let currentBlock = section?.blocks?.[blockIndex];
+    for (let index of nestedIndices) {
+      if (!currentBlock?.blocks) return null;
+      currentBlock = currentBlock.blocks[index];
+    }
+    return currentBlock;
+  };
+
+  const blockToRender = getBlockToRender(
+    currentIndexList[0],
+    currentIndexList.slice(1)
+  );
+
+  const renderBlockLabelPath = (
+    blockIndex: number,
+    nestedIndices: Array<number> = []
+  ): string => {
+    let currentBlock = section?.blocks?.[blockIndex];
+    let label = currentBlock?.label || "";
+    for (let index of nestedIndices) {
+      if (!currentBlock?.blocks) break;
+      currentBlock = currentBlock.blocks[index];
+      label += " -> " + (currentBlock?.label || "");
+    }
+    return label;
+  };
+
   const displayedQuestions = blockToRender?.questions.slice(
     (currentPage - 1) * questionsPerPage,
     currentPage * questionsPerPage
   );
 
-  // const handleContinue = () => {
-  //   console.log("Answers:", answers);
-  //   if (currentPage < totalPages) {
-  //     setCurrentPage(currentPage + 1);
-  //   } else if (nestedBlocks.length > 0 && !mainBlockQuestionsShown) {
-  //     // Main block questions have been shown, now show nested blocks
-  //     setMainBlockQuestionsShown(true);
-  //     setCurrentPage(1);
-  //   } else {
-  //     if (
-  //       isInsideNestedBlock &&
-  //       currentNestedBlockIndex < nestedBlocks.length - 1
-  //     ) {
-  //       // Move to the next nested block
-  //       setCurrentNestedBlockIndex(currentNestedBlockIndex + 1);
-  //       setCurrentPage(1);
-  //     } else if (currentBlockIndex < (section?.blocks?.length || 0) - 1) {
-  //       // Move to the next main block
-  //       setCurrentBlockIndex(currentBlockIndex + 1);
-  //       setMainBlockQuestionsShown(false); // Reset the flag for the new main block
-  //       setCurrentNestedBlockIndex(0); // Reset nested block index
-  //       setCurrentPage(1);
-  //     } else {
-  //       // Move to the next section
-  //       dispatch(incrementStep());
-  //       setCurrentBlockIndex(0);
-  //       setMainBlockQuestionsShown(false);
-  //       setCurrentNestedBlockIndex(0);
-  //       setCurrentPage(1);
-  //     }
-  //   }
-  // };
+  const totalQuestions = blockToRender?.questions.length || 0;
+  const totalPages = Math.ceil(totalQuestions / questionsPerPage);
+
+  // -----------------------------------------------------------------------------------------------------------------------------------
 
   const handleContinue = () => {
-    console.log("Answers:", answers);
-    const formattedAnswers = displayedQuestions?.map((question) => ({
+    console.log("Current Page:", currentPage);
+    console.log("Total Pages:", totalPages);
+    console.log("Current Index List:", currentIndexList);
+    console.log("Block to Render Blocks Length:", blockToRender?.blocks.length);
+
+    const formattedAnswers = displayedQuestions?.map((question: Question) => ({
       value: answers[question.uid],
       questionId: question.uid,
       businessPlanId: currentBusinessPlanId,
     }));
 
     console.log("Formatted Answers:", formattedAnswers);
-
     setIsBackDisabled(false);
 
+    const deepClone = [...currentIndexList];
+
     if (currentPage < totalPages) {
+      // If not the last page, simply move to the next page.
       setCurrentPage(currentPage + 1);
-    } else if (nestedBlocks.length > 0 && !mainBlockQuestionsShown) {
-      // Main block questions have been shown, now show nested blocks
-      setMainBlockQuestionsShown(true);
+    } else if (blockToRender?.blocks && blockToRender.blocks.length > 0) {
+      // If there are sub-blocks, transition to the first sub-block.
+      deepClone.push(0);
       setCurrentPage(1);
+      setCurrentIndexList(deepClone);
     } else {
-      if (
-        isInsideNestedBlock &&
-        currentNestedBlockIndex < nestedBlocks.length - 1
-      ) {
-        setCurrentNestedBlockIndex(currentNestedBlockIndex + 1);
-        setCurrentPage(1);
-      } else if (currentBlockIndex < (section?.blocks?.length || 0) - 1) {
-        setCurrentBlockIndex(currentBlockIndex + 1);
-        setMainBlockQuestionsShown(false); // Reset the flag for the new main block
-        setCurrentNestedBlockIndex(0); // Reset nested block index
-        setCurrentPage(1);
-      } else {
-        dispatch(incrementStep());
-        setCurrentBlockIndex(0);
-        setMainBlockQuestionsShown(false);
-        setCurrentNestedBlockIndex(0);
-        setCurrentPage(1);
-      }
+      // Move to the next block or sub-block.
+      moveNextBlockOrStep(deepClone);
     }
 
+    // Submit the answers
     if (formattedAnswers && currentBusinessPlanId) {
       dispatch(submitAnswersAsync(formattedAnswers as Answer[]));
     } else {
@@ -160,6 +140,37 @@ const Questions = () => {
     }
 
     triggerApiCalls();
+  };
+
+  const moveNextBlockOrStep = (indicesList: number[]) => {
+    let deepClone = [...indicesList];
+    const lastIdx = deepClone.length - 1;
+
+    // Increment the last index.
+    deepClone[lastIdx]++;
+
+    if (
+      lastIdx === 0 &&
+      deepClone[lastIdx] === (section?.blocks?.length || 0)
+    ) {
+      // If we are at the last main block, move to the next section or step.
+      dispatch(incrementStep());
+      setCurrentIndexList([0]);
+    } else if (
+      lastIdx > 0 &&
+      deepClone[lastIdx] ===
+        getBlockToRender(indicesList[0], indicesList.slice(1, lastIdx)).blocks
+          ?.length
+    ) {
+      // If we are at the last sub-block, move up a level and then call recursively.
+      deepClone.pop();
+      moveNextBlockOrStep(deepClone);
+    } else {
+      // Otherwise, just update to the incremented index list.
+      setCurrentIndexList(deepClone);
+    }
+
+    setCurrentPage(1);
   };
 
   const triggerApiCalls = () => {
@@ -220,48 +231,65 @@ const Questions = () => {
     }
   };
 
-  const handleBack = () => {
-    let newCurrentPage = currentPage;
-    let newCurrentBlockIndex = currentBlockIndex;
-    let newCurrentNestedBlockIndex = currentNestedBlockIndex;
+  const moveBackToLastSubBlock = (
+    indicesList: number[]
+  ): { indices: number[]; page: number } => {
+    let localClone = [...indicesList];
 
-    if (currentPage > 1) {
-      newCurrentPage = currentPage - 1;
-    } else if (isInsideNestedBlock && currentNestedBlockIndex > 0) {
-      // If inside a nested block and it's not the first nested block, move to the previous nested block
-      newCurrentNestedBlockIndex = currentNestedBlockIndex - 1;
-      newCurrentPage = Math.ceil(
-        (blockToRender?.questions?.length || 0) / questionsPerPage
-      );
-    } else if (currentBlockIndex > 0) {
-      // If it's a main block and not the first block, move to the previous block
-      newCurrentBlockIndex = currentBlockIndex - 1;
-      newCurrentNestedBlockIndex = 0; // Reset nested block index
-      newCurrentPage = Math.ceil(
-        (blockToRender?.questions?.length || 0) / questionsPerPage
-      );
-    }
+    const currentBlock = getBlockToRender(localClone[0], localClone.slice(1));
 
-    setCurrentPage(newCurrentPage);
-    setCurrentBlockIndex(newCurrentBlockIndex);
-    setCurrentNestedBlockIndex(newCurrentNestedBlockIndex);
+    let lastPage = Math.ceil(
+      (currentBlock.questions?.length || 0) / questionsPerPage
+    );
 
-    if (
-      newCurrentBlockIndex === 0 &&
-      !isInsideNestedBlock &&
-      newCurrentPage === 1
-    ) {
-      setIsBackDisabled(true);
+    if (lastPage > 0) {
+      return { indices: localClone, page: lastPage };
+    } else if (currentBlock.blocks && currentBlock.blocks.length > 0) {
+      // Navigate to the last sub-block
+      localClone.push(currentBlock.blocks.length - 1);
+      // Check if this sub-block has further nested sub-blocks
+      return moveBackToLastSubBlock(localClone);
     } else {
-      setIsBackDisabled(false);
+      return { indices: localClone, page: 1 };
     }
   };
 
-  const renderBlockLabel = () => {
-    if (isInsideNestedBlock) {
-      return `${currentBlock?.label} -> ${blockToRender?.label}`;
+  const handleBack = () => {
+    let currentIndices = [...currentIndexList];
+
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    } else {
+      if (currentIndices.length === 1) {
+        if (currentIndices[0] > 0) {
+          currentIndices[0]--;
+          const { indices, page } = moveBackToLastSubBlock(currentIndices);
+          currentIndices = indices;
+          setCurrentPage(page);
+        } else {
+          // We're at the very start, can't go back further
+          setIsBackDisabled(true);
+        }
+      } else {
+        const lastValue = currentIndices.pop();
+        if (lastValue && lastValue > 0) {
+          currentIndices.push(lastValue - 1);
+          const { indices, page } = moveBackToLastSubBlock(currentIndices);
+          currentIndices = indices;
+          setCurrentPage(page);
+        } else {
+          const { page } = moveBackToLastSubBlock(currentIndices);
+          setCurrentPage(page);
+        }
+      }
     }
-    return currentBlock?.label || "Commencer maintenant";
+
+    setCurrentIndexList(currentIndices);
+    setIsBackDisabled(
+      currentIndices.length === 1 &&
+        currentIndices[0] === 0 &&
+        currentPage === 1
+    );
   };
 
   const renderInputComponent = (inputType: InputType, question: Question) => {
@@ -370,16 +398,6 @@ const Questions = () => {
     );
   };
 
-  // console.log(
-  //   "Town is : ",
-  //   typeof answers["0f7b07a6-0a75-4df1-8e11-4e164712c7b4"]
-  // );
-
-  // console.log(
-  //   "Surface : ",
-  //   typeof Number(answers["4481644a-646e-486f-99b3-390535dc13b4"])
-  // );
-
   return (
     <div className="flex flex-col w-full sm:w-[470px] lg:w-[560px] min-[1864px]:w-[650px] h-full px-2">
       {loading ? (
@@ -400,7 +418,12 @@ const Questions = () => {
                   onClick={!isBackDisabled ? handleBack : undefined}
                 />
               </div>
-              <div className="text-[24px]">{renderBlockLabel()}</div>
+              <div className="text-[24px]">
+                {renderBlockLabelPath(
+                  currentIndexList[0],
+                  currentIndexList.slice(1)
+                )}
+              </div>
             </div>
             <div className="text-[#A08FB1] text-[16px] ps-[38px] mb-[28px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
@@ -409,16 +432,7 @@ const Questions = () => {
           </div>
           <div className="overflow-y-scroll py-[5px] qb-thumb h-[550px] mb-[10px]">
             <div className="mb-10 w-full sm:px-[35px]">
-              {/* {displayedQuestions?.map((question) => (
-                <div key={question.id} className="mb-6">
-                  <div className="px-[16px] mb-[12px] text-[14px] text-foundation-purple-dark-active">
-                    {question.label}
-                  </div>
-                  {renderInputComponent(question.inputType, question)}
-                </div>
-              ))}
-              <QuestionAiBox /> */}
-              {displayedQuestions?.map((question) => (
+              {displayedQuestions?.map((question: Question) => (
                 <div key={question.uid} className="mb-6">
                   <div className="px-[16px] mb-[12px] text-[14px] text-foundation-purple-dark-active">
                     {question.label}
