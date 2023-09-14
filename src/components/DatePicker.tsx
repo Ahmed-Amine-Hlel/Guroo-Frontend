e18036fc2751c254e13a62f44b13190c37ce63b5
Ahmed Hlel <kirigaya.kazuto.hlel@gmail.com>
2023-09-14 22:20:34 +0100

    boolean input component build fix 2

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-
 const DatePicker = () => {
-    return (
-        <div className="relative w-full">
-            <input
-                type="date"
-                className="w-[100%] text-start px-[12px] w-[100%] h-[45px] bg-light-p-hover border-[1px] 
+  return (
+    <div className="relative w-full">
+      <input
+        type="date"
+        className="w-[100%] text-start px-[12px] w-[100%] h-[45px] bg-light-p-hover border-[1px] 
                 border-foundation-purple-light-active bg-[#d9b2ff75] rounded-[8px] 
-                focus:outline-none text-[14px] text-[#41245eeb] input-date-icon" placeholder="Select date" />
-        </div>
-    )
-}
+                focus:outline-none text-[14px] text-[#41245eeb] input-date-icon"
+        placeholder="Select date"
+      />
+    </div>
+  );
+};
 
-export default DatePicker
+export default DatePicker;
