79d871aae045b24314a1682f8af3dece5c022564
01JAMIL <jamilbenbrahim1@gmail.com>
2023-09-16 22:54:21 +0100

    Few updates in MSTable component

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,13 +1,37 @@
-const DatePicker = ({className}: { className: string }) => {
-  return (
-      <input
-          type="date"
-          className={`${className} w-full text-start px-[12px] h-[45px] bg-light-p-hover border-[1px]
+import {ChangeEvent} from "react";
+
+interface DatePickerProps {
+    id?: string;
+    className?: string;
+    name?: string;
+    value?: string;
+    defaultValue?: string;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void
+}
+
+const getCurrentDate = (): string => {
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0'); // January is 0!
+    const dd = String(today.getDate()).padStart(2, '0');
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+const DatePicker = ({id, className, name, value, defaultValue = getCurrentDate(), onChange}: DatePickerProps) => {
+    return (
+        <input
+            id={id}
+            name={name}
+            type="date"
+            value={value}
+            defaultValue={defaultValue}
+            className={`w-full text-start px-[12px] h-[45px] bg-light-p-hover
                 border-foundation-purple-light-active bg-[#d9b2ff75]
-                focus:outline-none text-[12px] text-[#41245eeb] input-date-icon`}
-          placeholder="Select date"
-      />
-  );
+                focus:outline-none text-[12px] text-[#41245eeb] input-date-icon ${className}`}
+            placeholder="Select date"
+            onChange={onChange}
+        />
+    );
 };
 
 export default DatePicker;
--- src/components/DropDown.tsx
+++ src/components/DropDown.tsx
@@ -1,28 +1,26 @@
-import { Fragment, useState } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { FaChevronDown } from 'react-icons/fa';
+import {Fragment, SetStateAction ,Dispatch} from 'react'
+import {Menu, Transition} from '@headlessui/react'
+import {FaChevronDown} from 'react-icons/fa';
+
+interface DropDownProps {
+    items: string[];
+    selectedItem: string
+    setSelectedItem?: Dispatch<SetStateAction<string>>;
+}
 
 function classNames(...classes: any) {
     return classes.filter(Boolean).join(' ')
 }
 
-const DropDown = () => {
-
-    const [items] = useState<string[]>([
-        'CDI',
-        'Freelance',
-        'Stage',
-    ])
-
-    const [selectedItem, setSelectedItem] = useState<string>(items[0])
+const DropDown = ({items, selectedItem, setSelectedItem}: DropDownProps) => {
     return (
         <Menu as="div" className="w-full relative inline-block text-left">
             <div>
                 <Menu.Button className="flex items-center justify-between text-start px-[12px] w-[100%] h-[45px] bg-light-p-hover border-[1px] 
     border-foundation-purple-light-active bg-[#d9b2ff75] rounded-[8px] 
     focus:outline-none text-[14px] text-[#41245eeb]">
-                    {selectedItem}
-                    <FaChevronDown className="mr-1 h-[12px] w-[15px] text-[#572F7E] opacity-60" aria-hidden="true" />
+                    {selectedItem && selectedItem}
+                    <FaChevronDown className="mr-1 h-[12px] w-[15px] text-[#572F7E] opacity-60" aria-hidden="true"/>
                 </Menu.Button>
             </div>
 
@@ -35,19 +33,20 @@ const DropDown = () => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items
+                    className="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
                         {
-                            items.map((item, index) => (
+                            items && items.map((item, index) => (
                                 <Menu.Item key={index}>
-                                    {({ active }) => (
+                                    {({active}) => (
                                         <div
                                             className={classNames(
                                                 active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                                 'block px-4 py-2 text-sm cursor-pointer'
                                             )}
                                             onClick={() => {
-                                                setSelectedItem(item)
+                                                setSelectedItem?.(item)
                                             }}
                                         >
                                             {item}
