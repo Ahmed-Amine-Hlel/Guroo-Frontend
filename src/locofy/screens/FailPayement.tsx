d39c0a9f7a033bb1226146186f5911b4c2d2cc30
Ahmed Hlel <kirigaya.kazuto.hlel@gmail.com>
2023-10-11 11:47:26 +0100

    payment, fail routes upadte 2

--- src/locofy/screens/FailPayement.tsx
+++ src/locofy/screens/FailPayement.tsx
@@ -1,30 +1,40 @@
+// import { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+
 const FailPayement = () => {
-    return (
-        <div className='transition-all ease-in-out duration-400 flex min-h-screen p-4 sm:p-10 bg-[#FF8383]'>
-            <div
-                className='flex justify-center items-center w-full bg-[#FFEFEF] rounded-[16px]'
+  //   const navigate = useNavigate();
 
-            >
-                <div className='relative flex flex-col gap-[10px] items-center'>
-                    <img
-                        src="fail-payement.svg"
-                        alt='logo'
-                        className='object-cover w-[300px] md:w-[350px] mb-[16px]'
-                    />
-                    <div className='font-plus-jakarta-sans text-[#DB5454] text-[16px] md:text-[24px] font-medium'>
-                        Votre paiement à échoué
-                    </div>
-                    <button className="absolute -right-1 bottom-[85px] md:-right-4 md:bottom-24">
-                        <img
-                            alt="close"
-                            src="fail-close-btn.png"
-                            className="w-[60px] md:w-[80px]"
-                        />
-                    </button>
-                </div>
-            </div>
+  //   useEffect(() => {
+  //     const timer = setTimeout(() => {
+  //       navigate("/business-plan");
+  //     }, 3000);
+
+  //     return () => clearTimeout(timer);
+  //   }, [navigate]);
+
+  return (
+    <div className="transition-all ease-in-out duration-400 flex min-h-screen p-4 sm:p-10 bg-[#FF8383]">
+      <div className="flex justify-center items-center w-full bg-[#FFEFEF] rounded-[16px]">
+        <div className="relative flex flex-col gap-[10px] items-center">
+          <img
+            src="fail-payement.svg"
+            alt="logo"
+            className="object-cover w-[300px] md:w-[350px] mb-[16px]"
+          />
+          <div className="font-plus-jakarta-sans text-[#DB5454] text-[16px] md:text-[24px] font-medium">
+            Votre paiement à échoué
+          </div>
+          <button className="absolute -right-1 bottom-[85px] md:-right-4 md:bottom-24">
+            <img
+              alt="close"
+              src="fail-close-btn.png"
+              className="w-[60px] md:w-[80px]"
+            />
+          </button>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default FailPayement
+export default FailPayement;
