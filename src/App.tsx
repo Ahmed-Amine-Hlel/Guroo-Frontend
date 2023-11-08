a2b8f798c2b7ac6e12929011595e5dfdcf882f70
Ahmed Hlel <kirigaya.kazuto.hlel@gmail.com>
2023-11-08 11:34:38 +0100

    payment/fail changes as well as vercel.json file addition

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
-import { GoogleOAuthProvider } from "@react-oauth/google";
-import { Route, Routes, useLocation } from "react-router-dom";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import AuthScreen from "./locofy/screens/AuthScreen";
-import BusinessPlanMenu from "./locofy/screens/BusinessPlanMenu";
-import CreateBusinessPlan from "./locofy/screens/CreateBusinessPlan";
-import EditBusinessPlan from "./locofy/screens/EditBusinessPlan";
-import FailPayement from "./locofy/screens/FailPayement";
-import NewBusinessPlan from "./locofy/screens/NewBusinessPlan";
-import PayementScreen from "./locofy/screens/PayementScreen";
-import PrePayementScreen from "./locofy/screens/PrePayementScreen";
-import SuccessPayement from "./locofy/screens/SuccessPayement";
-import MSTable from "./components/MSTable";
-import CFTable from "./components/CFTable";
-// import MSTable from "./components/MSTable";
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import AuthScreen from './locofy/screens/AuthScreen';
+import BusinessPlanMenu from './locofy/screens/BusinessPlanMenu';
+import CreateBusinessPlan from './locofy/screens/CreateBusinessPlan';
+import EditBusinessPlan from './locofy/screens/EditBusinessPlan';
+import FailPayement from './locofy/screens/FailPayement';
+import NewBusinessPlan from './locofy/screens/NewBusinessPlan';
+import PayementScreen from './locofy/screens/PayementScreen';
+import PrePayementScreen from './locofy/screens/PrePayementScreen';
+import SuccessPayement from './locofy/screens/SuccessPayement';
 
 function App() {
   const location = useLocation();
 
-  const pathsWithoutNavbar = ["/payment/:businessPlanId", "/pre-payment"];
+  const pathsWithoutNavbar = [
+    '/payment/:businessPlanId',
+    '/pre-payment',
+    '/success-payment',
+    '/fail-payment',
+  ];
 
   const isPathInList = (path: string, pathsList: string[]) => {
-    const segments = path.split("/").filter(Boolean);
+    const segments = path.split('/').filter(Boolean);
     for (const knownPath of pathsList) {
-      const knownSegments = knownPath.split("/").filter(Boolean);
+      const knownSegments = knownPath.split('/').filter(Boolean);
       if (segments.length !== knownSegments.length) continue;
       let match = true;
       for (let i = 0; i < segments.length; i++) {
-        if (knownSegments[i].startsWith(":")) continue;
+        if (knownSegments[i].startsWith(':')) continue;
         if (segments[i] !== knownSegments[i]) {
           match = false;
           break;
@@ -45,30 +47,26 @@ function App() {
       <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID!}>
         {!shouldHideNavbar && <Navbar />}
         <Routes>
-          <Route path={"/"} element={<AuthScreen />} />
-          <Route path={"/business-plan"} element={<BusinessPlanMenu />} />
+          <Route path={'/'} element={<AuthScreen />} />
+          <Route path={'/business-plan'} element={<BusinessPlanMenu />} />
           <Route
-            path={"/create-business-plan"}
+            path={'/create-business-plan'}
             element={<CreateBusinessPlan />}
           />
-          <Route path={"/new-business-plan"} element={<NewBusinessPlan />} />
+          <Route path={'/new-business-plan'} element={<NewBusinessPlan />} />
           <Route
-            path={"/payment/:businessPlanId"}
+            path={'/payment/:businessPlanId'}
             element={<PayementScreen />}
           />
-          <Route path={"/pre-payment"} element={<PrePayementScreen />} />
+          <Route path={'/pre-payment'} element={<PrePayementScreen />} />
           <Route
-            path={"/edit-business-plan/:uid"}
+            path={'/edit-business-plan/:uid'}
             element={<EditBusinessPlan />}
           />
-          <Route path={"/success-payment"} element={<SuccessPayement />} />
-          <Route path={"/fail-payment"} element={<FailPayement />} />
+          <Route path={'/success-payment'} element={<SuccessPayement />} />
+          <Route path={'/fail-payment'} element={<FailPayement />} />
         </Routes>
       </GoogleOAuthProvider>
-      {/*<MSTable />
-      <CFTable /> */}
-      {/* <MSTable handleNext={() => {}} /> */}
-      {/* <CFTable /> */}
     </div>
   );
 }
--- src/locofy/screens/SuccessPayement.tsx
+++ src/locofy/screens/SuccessPayement.tsx
@@ -1,23 +1,33 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const SuccessPayement = () => {
-    return (
-        <div className='transition-all ease-in-out duration-400 flex min-h-screen p-4 sm:p-10 bg-gradient-to-br from-[#914fd2] from-0% to-[#a56be1] to-100%'>
-            <div
-                className='flex justify-center items-center w-full bg-[#f4edfb] rounded-[16px]'
+  const navigate = useNavigate();
 
-            >
-                <div className='flex flex-col gap-[10px] items-center'>
-                    <img
-                        src="payement-success.svg"
-                        alt='logo'
-                        className='object-cover w-[300px] md:w-[350px] mb-[16px]'
-                    />
-                    <div className='font-plus-jakarta-sans text-[#572f7e] text-[16px] md:text-[24px] font-medium'>
-                        Vous venez d’arriver au bout des questions
-                    </div>
-                </div>
-            </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigate('/business-plan');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
+  return (
+    <div className="transition-all ease-in-out duration-400 flex min-h-screen p-4 sm:p-10 bg-gradient-to-br from-[#914fd2] from-0% to-[#a56be1] to-100%">
+      <div className="flex justify-center items-center w-full bg-[#f4edfb] rounded-[16px]">
+        <div className="flex flex-col gap-[10px] items-center">
+          <img
+            src="payement-success.svg"
+            alt="logo"
+            className="object-cover w-[300px] md:w-[350px] mb-[16px]"
+          />
+          <div className="font-plus-jakarta-sans text-[#572f7e] text-[16px] md:text-[24px] font-medium">
+            Vous venez d’arriver au bout des questions
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default SuccessPayement
+export default SuccessPayement;
