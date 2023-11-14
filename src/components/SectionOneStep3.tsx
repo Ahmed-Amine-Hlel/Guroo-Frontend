083ea5cfeb1366e7139d61c266dbef9d193b168d
01JAMIL <jamilbenbrahim1@gmail.com>
2023-11-14 12:31:40 +0100

    Implement handleSelect on the InputGooglePlaces

--- src/components/SectionOneStep3.tsx
+++ src/components/SectionOneStep3.tsx
@@ -1,11 +1,11 @@
-import { HiMiniArrowLeft } from 'react-icons/hi2';
-import InputMultiUnitNumber from './InputMultiUnitNumber';
-import InputGooglePlaces from './InputGooglePlaces';
+import { HiMiniArrowLeft } from "react-icons/hi2";
+import InputMultiUnitNumber from "./InputMultiUnitNumber";
+import InputGooglePlaces from "./InputGooglePlaces";
 import {
   useAppDispatch,
   // useAppSelector,
-} from '../hooks/hooks';
-import { setAnswer } from '../store/answersSlice';
+} from "../hooks/hooks";
+import { setAnswer } from "../store/answersSlice";
 
 const SectionOneStep3 = ({
   currentBusinessPlanId,
@@ -20,10 +20,14 @@ const SectionOneStep3 = ({
 
   // console.log("answers : ", answers);
 
-  const handleInputChange = (rowNumber: string, value: unknown) => {
+  const handleSelectOption = (rowNumber: string, value: unknown) => {
+   /*  console.log("rowNumber : ", rowNumber);
+    console.log("value : ", value);
+    console.log(rowNumber.split("-")[0]);
+    console.log(rowNumber.split("-")[1]); */
     dispatch(
       setAnswer({
-        rowNumber,
+        rowNumber: rowNumber.split("-")[0],
         value,
         businessPlanId: currentBusinessPlanId,
       })
@@ -36,7 +40,7 @@ const SectionOneStep3 = ({
         <div className="flex items-center gap-[12px] text-[#6D3B9E] mb-[8px]">
           <div>
             <HiMiniArrowLeft
-              className={`text-[24px] ${'hover:cursor-pointer'}`}
+              className={`text-[24px] ${"hover:cursor-pointer"}`}
               onClick={handleBack}
               // onClick={!isBackDisabled ? handleBack : undefined}
             />
@@ -58,7 +62,7 @@ const SectionOneStep3 = ({
             Où sera situé votre restaurant ?
           </label>
 
-          <InputGooglePlaces />
+          <InputGooglePlaces handleSelectOption={handleSelectOption} />
           {/* <StringInput value={""} onChange={() => console.log("onChange")} /> */}
           <div className="px-[16px] mt-[8px] text-[#41245E] opacity-50 text-[14px]">
             L'adresse nous donnera des informations clés sur les salaires, le
@@ -72,8 +76,8 @@ const SectionOneStep3 = ({
           </label>
 
           <InputMultiUnitNumber
-            value={'0'}
-            onChange={() => console.log('onChange')}
+            value={"0"}
+            onChange={() => console.log("onChange")}
           />
         </div>
       </div>
